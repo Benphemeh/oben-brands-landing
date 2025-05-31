2890d3866c6cffb84475a2ff21ea0d3abf1ee3df
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-05-31 07:59:08 +0100

    favicon created

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'O’Ben Brands | Fresh Porks & Provisions',
+  title: 'O’Ben Brands  Fresh Porks & Provisions',
   description: 'Farm-fresh meat and everyday essentials delivered to your doorstep.',
   generator: 'Next.js',
+    icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
