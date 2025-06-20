3074729cb7fa9741000bfd69af18299e61e20069
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-06-20 10:42:23 +0100

    feat: provision AWS infrastructure with Terraform and prepare Kubernetes manifests
    - Added Terraform configs for VPC, subnet, security group, and EC2 instances
    - Configured outputs for instance IDs and IPs
    - Created .gitignore to exclude sensitive and build files
    - Added initial Kubernetes manifests: deployment, service, ingress, and secret
    - Set up SSH key pair for EC2 access

--- project-name/infra/aws/variables.tf
+++ project-name/infra/aws/variables.tf
@@ -25,10 +25,28 @@ variable "subnet_cidr" {
 variable "key_name" {
   description = "The name of the key pair to use for SSH access"
   type        = string
+  default     = "oben-brands-key"
 }
 
 variable "desired_capacity" {
   description = "The desired number of instances in the Auto Scaling group"
   type        = number
   default     = 1
+}
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI for us-east-1
 }
